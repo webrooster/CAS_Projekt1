7c965fff9cd80440ad688f952227f4b6e01ec10a
Roli <webrooster@webrooster.ch>
2020-04-23 17:24:41 +0200

    Separate datas from NotesController

---
+++ components/datas.js
@@ -0,0 +1,28 @@
+/**
+ * @desc Static note datas
+ * @author: Roland von Aesch
+ */
+
+export const notesDatas = [
+    {
+        'title': 'Znüni Brötli parat machen',
+        'description': 'Lorem ipsum dolor sit amet ...',
+        'created_at': '12-04-2020',
+        'expire': '16-04-2020',
+        'importance': 5,
+    },
+    {
+        'title': 'Bla bla bla',
+        'description': 'Bla bla bla bla bla bla ...',
+        'created_at': '10-03-2020',
+        'expire': '26-03-2020',
+        'importance': 2,
+    },
+    {
+        'title': 'Noch ein Task',
+        'description': 'Eine kleine Beschreibung dazu',
+        'created_at': '09-02-2020',
+        'expire': '06-03-2020',
+        'importance': 2,
+    },
+]
