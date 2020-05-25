891fe2ebfdb852661a74270a8eb2eb4434663fd5
webrooster <roland.vonaesch@fhnw.ch>
2020-05-25 16:43:45 +0200

    Adding cancel button to form, form submit prevent default, styling reset and submit button

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,3 +5,16 @@ document.addEventListener("DOMContentLoaded", function (event) {
         document.body.classList.toggle('theme__dark');
     });
 });
+
+const $formsubmit = document.querySelector('#submit');
+const $formreset = document.querySelector('#reset');
+
+$formsubmit.addEventListener('click', (e) => {
+    e.preventDefault();
+    alert('Form submit clicked!')
+});
+
+$formreset.addEventListener('click', (e) => {
+    e.preventDefault();
+    alert('Form reset clicked!')
+});
