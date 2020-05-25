e83a274841b8172afe04d94eee60bb3c312ebf12
webrooster <roland.vonaesch@fhnw.ch>
2020-05-25 17:29:30 +0200

    Change default theme, add event listener to edit button

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,6 +8,19 @@ document.addEventListener("DOMContentLoaded", function (event) {
 
 const $formsubmit = document.querySelector('#submit');
 const $formreset = document.querySelector('#reset');
+const $editnote = document.querySelector('.noteslist');
+
+$editnote.addEventListener('click', e => {
+    if (event.target.classList[0] == 'edit') {
+        const dataIndex = event.target.parentElement.parentElement.parentElement.getAttribute('data-index');
+        console.log('dataIndex', e.target, dataIndex);
+
+    } else if (event.target.classList[1] == 'fa-edit') {
+        const dataIndex = event.target.parentElement.parentElement.parentElement.parentElement.getAttribute('data-index');
+        console.log('dataIndex', e.target, dataIndex);
+
+    }
+});    
 
 $formsubmit.addEventListener('click', (e) => {
     e.preventDefault();
