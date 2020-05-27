eae803bee2a2763308c5428223253c17208299bd
webrooster <roland.vonaesch@fhnw.ch>
2020-05-27 09:08:59 +0200

    Add sort buttons dom elements

--- src/js/views/base.js
+++ src/js/views/base.js
@@ -1,7 +1,7 @@
 export const DOM = {
 
     /**
-     * Form field elements
+     * UI ELEMENTS
      */
 
     noteform: document.getElementById('note__form'),
@@ -12,10 +12,10 @@ export const DOM = {
     submit: document.querySelector('#submit'),
     reset: document.querySelector('#clear'),
     theme__toggler: document.querySelector('#theme__toggler'),
-    standard__list: document.querySelector('#standard__list')
-
-    /**
-     * UI buttons
-     */
-
+    standard__list: document.querySelector('#standard__list'),
+    sort_createdAt: document.querySelector('#sort_createdAt'),
+    sort_importance: document.querySelector('#sort_importance'),
+    sort_finished: document.querySelector('#sort_finished'),
+    sort_finished_date: document.querySelector('#sort_finished_date'),
+    
 }
