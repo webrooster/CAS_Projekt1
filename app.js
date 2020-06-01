6dfbe44a12b329e469c1e144f52f1b4bec7b236c
webrooster <roland.vonaesch@fhnw.ch>
2020-06-01 19:04:30 +0200

    Styling form and height grid col 1

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.static(path.join(__dirname, 'public'), {index: 'zoo.html'}));
+app.use(express.static(path.join(__dirname, 'public'), {index: 'index.html'}));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
