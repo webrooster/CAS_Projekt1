73470e2321947f2b48203f386a9f0a25439ec62c
webrooster <roland.vonaesch@fhnw.ch>
2020-06-02 18:27:48 +0200

    Cleanup testing code

--- public/assets/js/models/note.js
+++ public/assets/js/models/note.js
@@ -30,41 +30,10 @@ export class Note {
       }
     }
 
-    // FORMAT DATE
-    // formatDate() {
-    //   const dateCreated = new Date();
-    //   const timeZone = dateCreated + dateCreated.getTimezoneOffset();
-    //   const time = new Date(timeZone).toUTCString();
-
-    //   console.log('TIMEZONE', time);
-
-    //   return time;
-    // }
-
-    // convertExpireDate(expire) {
-    //   const convertGermanDate = new Date(expire); //.toLocaleString('de-DE');
-    //   const dateTimeZone = new Date(convertGermanDate);
-    //   const date = new Date(dateTimeZone).toLocaleString('de-DE');
-    //   console.log('EXPIRE', new Date(dateTimeZone).toLocaleString('de-DE'), 'dateTimeZone', new Date(dateTimeZone).getTimezoneOffset());
-    //   return date;  
-    // }
-
     currentDate() {
       let today = new Date().toLocaleString('de-DE');
       return today;
     }
-
-    // currentDate() {
-    //   let today = new Date();
-    //   let seconds = String(today.getSeconds());
-    //   let minutes = String(today.getMinutes());
-    //   let hours = String(today.getHours());
-    //   let dd = String(today.getDate()).padStart(2, '0');
-    //   let mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
-    //   let yyyy = today.getFullYear();
-    //   today = `${ dd }.${ mm }.${ yyyy }, at ${hours}:${minutes}:${seconds}`;
-    //   return today;
-    // }
 
     randomId() {
         return ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, c =>
