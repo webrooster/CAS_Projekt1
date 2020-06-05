941d9c35f1c4c24d69156c809aeaea0405dea0d3
webrooster <roland.vonaesch@fhnw.ch>
2020-06-05 19:20:07 +0200

    Adding folder models and schema

---
+++ models/note.schema.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+// NOTE SCHEMA
+module.exports = notesSchema = new mongoose.Schema({
+    title: { type: String, required: true },
+    description: { type: String},
+    created: { type: String, required: true },
+    importance: { type: Number, required: true},
+    expire: { type: String, required: true },
+    complete: { type: Boolean, required: true },
+    completed_at: { type: String, required: true }
+});
