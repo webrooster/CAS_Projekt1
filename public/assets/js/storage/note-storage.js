045476b1870cbf26fc4067bdf5b398561f32bfb9
webrooster <roland.vonaesch@fhnw.ch>
2020-06-10 11:06:10 +0200

    CRUD DELETE -> remove req body

--- public/assets/js/storage/note-storage.js
+++ public/assets/js/storage/note-storage.js
@@ -78,13 +78,11 @@ export class NoteStorage {
           method: 'DELETE',
           headers: {
             'Content-Type': 'application/json',
-          },
-        //   body: JSON.stringify(note),
+          }
         };
         try {
             const url = `http://localhost:3000/notes/${note}`;
             const response = await fetch(url, options);
-            // const note = await response.json();
             return url;
           
         } catch (err) {
