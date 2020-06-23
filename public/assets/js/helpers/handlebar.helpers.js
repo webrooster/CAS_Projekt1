6128774888dee5923774abd4ca54af4af4e3e129
webrooster <roland.vonaesch@fhnw.ch>
2020-06-23 11:15:57 +0200

    Handlebar helper functions -> comments

--- public/assets/js/helpers/handlebar.helpers.js
+++ public/assets/js/helpers/handlebar.helpers.js
@@ -1,11 +1,11 @@
 import * as helper from './helper.functions.js';
 
-// HANDLEBAR HELPER - CREATED
+// HANDLEBAR HELPER - CONVERT CREATED DATE
 export const handlebar_helper_created = Handlebars.registerHelper('formatTime', (created) => {
     return new Date(created).toLocaleString(helper.getBrowserLanguage());
 });
 
-// HANDLEBAR HELPER - CONVERT COMPLETED_AT
+// HANDLEBAR HELPER - CONVERT COMPLETED_AT DATE
 export const handlebar_helper_completed_at = Handlebars.registerHelper('formatTime', (completed_at) => {
     return new Date(completed_at).toLocaleString(helper.getBrowserLanguage());
 });
@@ -14,11 +14,6 @@ export const handlebar_helper_completed_at = Handlebars.registerHelper('formatTi
 export const handlebar_helper_expire = Handlebars.registerHelper('formatExpire', (expire) => {
     const expireLocalTime = expire.toLocaleString(helper.getBrowserLanguage());
     return new Date(expireLocalTime).toLocaleDateString(helper.getBrowserLanguage());
-});
-
-// HANDLEBAR HELPER - SORTING STATE
-export const handlebar_equals = Handlebars.registerHelper('ifEquals', function(arg1, arg2, options) {
-    return (arg1 == arg2) ? options.fn(this) : options.inverse(this);
 });
 
 // SET IMPORTANCE VALUE
@@ -30,4 +25,9 @@ export const handlebar_select = Handlebars.registerHelper('select', (importance,
             return RegExp(t).test(v) ? v.replace(t, t + ' selected="selected"') : v;
         })
         .join('\n');
+});
+
+// HANDLEBAR HELPER - SORTING STATE
+export const handlebar_equals = Handlebars.registerHelper('ifEquals', function(arg1, arg2, options) {
+    return (arg1 == arg2) ? options.fn(this) : options.inverse(this);
 });
